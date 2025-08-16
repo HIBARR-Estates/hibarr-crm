d5a85e34cea2a1307d703a5ecc15683a458f4cbf
Ayomide Oluniyi <ayorg360@gmail.com>
2025-08-16 18:28:27 +0300

    Refactor layout of card and form components for improved consistency
    Updated the layout of card headers and form fields by removing unnecessary padding and adjusting margin classes for better alignment. Enhanced the structure of custom fields in various views to ensure uniformity across the application.

--- resources/views/leads/ajax/show.blade.php
+++ resources/views/leads/ajax/show.blade.php
@@ -53,7 +53,7 @@ $viewLeadFollowupPermission = user()->permission('view_lead_follow_up');
                         <x-status style="color: {{ $deal->leadStage->label_color }}" color="yellow"
                                     :value="$deal->leadStage->name"/>
                     </p>
-                    <div class="row">
+                    <div class="row mx-0">
 
                         <x-cards.data-row :label="__('modules.deal.dealName')" :value="$deal->name ?? '--'"/>
         
@@ -117,7 +117,7 @@ $viewLeadFollowupPermission = user()->permission('view_lead_follow_up');
             <div id="custom-fields-category-container">
                 @if (isset($customFieldCategories) && count($customFieldCategories) > 0)
                     @foreach ($customFieldCategories as $category)
-                        <div class="row custom-fields-category-container"
+                        <div class="row custom-fields-category-container mx-0"
                             id="custom-fields-category-{{ $category->id }}" style="display: none;">
                             <x-forms.custom-field-show :fields="$fields" :model="$deal"
                                 :categoryId="$category->id"></x-forms.custom-field-show>
