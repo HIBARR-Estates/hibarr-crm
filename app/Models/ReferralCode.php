bf695ceca5f8c0a563ff3fb4fef72afea81f5209
Kaasi Kodes <thisbroisfresh@gmail.com>
2025-09-09 06:40:21 +0100

    chore: adds a generateCode and getReferralLink methods in the ReferralCode model

--- app/Models/ReferralCode.php
+++ app/Models/ReferralCode.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReferralCode extends Model
 {
@@ -19,8 +20,32 @@ class ReferralCode extends Model
     /**
      * Get the employee details associated with this referral code.
      */
-    public function employee()
+    public function employee(): BelongsTo
     {
         return $this->belongsTo(EmployeeDetails::class, 'employee_id');
+    }
+
+
+    
+    /**
+     * Generate a new unique referral code.
+     */
+    public static function generateCode(): string
+    {
+        // Get the company prefix from settings if needed, or from the employee's company
+        $companyPrefix = company() ? strtoupper(substr(company()->name, 0, 3)) . '-' : '';
+        
+        do {
+            $code = $companyPrefix . strtoupper(Str::random(8));
+        } while (self::where('referral_code', $code)->exists());
+
+        return $code;
+    }
+
+    // getReferralLink
+    public function getReferralLink(): string
+    {
+        // TODO: Ensure that a referral route is fleshed out
+        return url('/referral/' . $this->referral_code);
     }
 }
