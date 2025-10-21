f254d772887f01dcf519e6a41c595fb9f6af88f6
Kaasi Kodes <thisbroisfresh@gmail.com>
2025-10-21 06:13:01 +0100

    fix: update properties menu link to point to the correct route

--- resources/views/sections/menu.blade.php
+++ resources/views/sections/menu.blade.php
@@ -202,7 +202,7 @@
 
 <!-- NAV ITEM - PROPERTIES -->
 @if (!in_array('purchase', user_modules()) && in_array('products', user_modules()) && $sidebarUserPermissions['view_product'] != 5 && $sidebarUserPermissions['view_product'] != 'none')
-        <x-menu-item icon="house-door" :text="__('app.menu.properties')" :link="route('products.index')">
+        <x-menu-item icon="house-door" :text="__('app.menu.properties')" :link="route('properties.index')">
             <x-slot name="iconPath">
                 <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793l-2.293-2.293z"/>
                 <path d="M13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207L8 2.207l5 5z"/>
