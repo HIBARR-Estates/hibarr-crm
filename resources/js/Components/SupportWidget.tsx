482783aa5ed4b7c94c08b9287cc6b77531d06512
Kaasi Kodes <thisbroisfresh@gmail.com>
2025-12-04 21:06:58 +0100

    feat: Add SupportWidget component for user support and feedback
    Closes HIB-34

--- resources/js/Components/SupportWidget.tsx
+++ resources/js/Components/SupportWidget.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import { FloatButton, Popover, List } from 'antd';
-import { QuestionCircleOutlined, BugOutlined, BulbOutlined } from '@ant-design/icons';
+import React from "react";
+import { FloatButton, Popover, List } from "antd";
+import {
+    QuestionCircleOutlined,
+    BugOutlined,
+    BulbOutlined,
+} from "@ant-design/icons";
 
 const SupportWidget: React.FC = () => {
     const content = (
@@ -10,21 +14,21 @@ const SupportWidget: React.FC = () => {
                 split={false}
                 dataSource={[
                     {
-                        title: 'Report Bugs',
+                        title: "Report Bugs",
                         icon: <BugOutlined />,
-                        url: 'https://hibarr-dev.atlassian.net/jira/software/form/04cb685a-c280-4a27-a1d4-b708de106630?from=directory'
+                        url: "https://hibarr-dev.atlassian.net/jira/software/form/04cb685a-c280-4a27-a1d4-b708de106630?from=directory",
                     },
                     {
-                        title: 'Request Features',
+                        title: "Request Features",
                         icon: <BulbOutlined />,
-                        url: 'https://hibarr-dev.atlassian.net/jira/software/form/474174e9-559d-46bb-a504-d7df04eef2af?from=directory'
-                    }
+                        url: "https://hibarr-dev.atlassian.net/jira/software/form/474174e9-559d-46bb-a504-d7df04eef2af?from=directory",
+                    },
                 ]}
                 renderItem={(item) => (
                     <List.Item className="!px-0">
-                        <a 
-                            href={item.url} 
-                            target="_blank" 
+                        <a
+                            href={item.url}
+                            target="_blank"
                             rel="noopener noreferrer"
                             className="flex items-center gap-3 text-gray-700 hover:text-blue-600 w-full px-2 py-1 rounded hover:bg-gray-50 transition-colors"
                         >
@@ -38,15 +42,15 @@ const SupportWidget: React.FC = () => {
     );
 
     return (
-        <Popover 
-            content={content} 
-            title="Support & Feedback" 
+        <Popover
+            content={content}
+            title="Support & Feedback"
             trigger="click"
             placement="topRight"
         >
-            <FloatButton 
-                icon={<QuestionCircleOutlined />} 
-                type="primary" 
+            <FloatButton
+                icon={<QuestionCircleOutlined />}
+                type="primary"
                 style={{ right: 24, bottom: 24 }}
                 tooltip="Support"
             />
