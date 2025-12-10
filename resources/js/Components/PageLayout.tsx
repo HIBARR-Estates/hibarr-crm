956d10ded584ff0cd70e273c763f35ff36579686
Kaasi Kodes <thisbroisfresh@gmail.com>
2025-12-10 12:32:57 +0100

    chore: Ensure the user's role is displayed on PageLayout

--- resources/js/Components/PageLayout.tsx
+++ resources/js/Components/PageLayout.tsx
@@ -156,10 +156,11 @@ export default function PageLayout({
                                     </Avatar>
                                     <div className="flex flex-col">
                                         <span className="text-sm font-medium truncate">
-                                            {appName}
+                                            {user?.name}
                                         </span>
                                         <span className="text-xs text-gray-500 truncate">
-                                            {user?.name}
+                                            {user?.roles?.[0]?.display_name ??
+                                                appName}
                                         </span>
                                     </div>
                                 </div>
