8a14c33b4970987cb8a0fd7369a739be853a68f7
Kaasi Kodes <74901471+kaasikodes@users.noreply.github.com>
2025-12-29 21:30:33 +0100

    chore: ensures deletet and edit for tasks visible to the authenticated user

--- resources/js/Features/Tasks/Columns/index.tsx
+++ resources/js/Features/Tasks/Columns/index.tsx
@@ -301,6 +301,7 @@ export const useTasksTableColumns = ({
                         icon: <EditOutlined />,
                         label: "Edit Task",
                         onClick: () => onEdit(record),
+                        //
                         // disabled: !canEdit,
                     },
                     {
