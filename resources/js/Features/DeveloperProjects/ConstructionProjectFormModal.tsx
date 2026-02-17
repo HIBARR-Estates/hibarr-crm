d0cf15ecc7d00f074b07a7a80964f8a1e25be926
Kaasi Kodes <74901471+kaasikodes@users.noreply.github.com>
2026-02-17 16:31:20 +0100

    feat: add project-level assets routes for managing images and videos

--- resources/js/Features/DeveloperProjects/ConstructionProjectFormModal.tsx
+++ resources/js/Features/DeveloperProjects/ConstructionProjectFormModal.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useCallback } from "react";
+import dayjs from "dayjs";
 import { Form, Modal, Input, message, Skeleton } from "antd";
 import {
     BuildOutlined,
@@ -140,7 +141,9 @@ const ConstructionProjectFormModal: React.FC<
                 number_of_blocks: project.number_of_blocks,
                 project_total_area_sqm: project.project_total_area_sqm,
                 construction_status: project.construction_status,
-                completion_date: project.completion_date,
+                completion_date: project.completion_date
+                    ? dayjs(project.completion_date)
+                    : undefined,
                 number_of_phases: project.number_of_phases,
                 furniture_package: project.furniture_package,
                 rental_guarantee: project.rental_guarantee,
@@ -167,6 +170,9 @@ const ConstructionProjectFormModal: React.FC<
 
                 const submitData = {
                     ...cleanData,
+                    completion_date: cleanData.completion_date
+                        ? cleanData.completion_date.format("YYYY-MM-DD")
+                        : null,
                     primary_categories: cleanData.primary_categories || [],
                     unit_types: cleanData.unit_types || [],
                     facilities: cleanData.facilities || [],
