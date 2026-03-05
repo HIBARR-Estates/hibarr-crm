c8605ad7097e491bee89906b01c20d22829e5659
Kaasi Kodes <74901471+kaasikodes@users.noreply.github.com>
2026-03-05 18:04:00 +0100

    feat(location): wrap address in JSON structure if provided as a plain string

--- resources/js/Features/DeveloperProjects/ConstructionProjectFormModal.tsx
+++ resources/js/Features/DeveloperProjects/ConstructionProjectFormModal.tsx
@@ -198,7 +198,10 @@ const ConstructionProjectFormModal: React.FC<
                 ? {
                       city: (project.location as any).city ?? undefined,
                       area: (project.location as any).area ?? undefined,
-                      address: (project.location as any).address ?? undefined,
+                      address:
+                          (project.location as any)?.address?.street ??
+                          (project.location as any)?.address ??
+                          undefined,
                       latitude: (project.location as any).latitude ?? undefined,
                       longitude:
                           (project.location as any).longitude ?? undefined,
