00bd579e933dfd4e833e2b2afdf4cb3d730dc894
Kaasi Kodes <74901471+kaasikodes@users.noreply.github.com>
2026-03-27 07:40:48 +0100

    refactor: Add project prop to ConstructionProjectLocationSection and set initial area value

--- resources/js/Features/Properties/SaveProperty/sections/ConstructionProjectLocationSection.tsx
+++ resources/js/Features/Properties/SaveProperty/sections/ConstructionProjectLocationSection.tsx
@@ -4,12 +4,14 @@ import type { FormInstance } from "antd/lib/form";
 import type { PropertyEnumValues } from "@/Types";
 import { usePage } from "@inertiajs/react";
 import { DISTANCE_FIELDS } from "../constructionProjectConfig";
+import { DeveloperProject } from "@/Types/developerProject";
 
 const { TextArea } = Input;
 
 interface ConstructionProjectLocationSectionProps {
     form: FormInstance;
     enumValues?: PropertyEnumValues;
+    project?: DeveloperProject;
 }
 
 /**
@@ -21,7 +23,7 @@ interface ConstructionProjectLocationSectionProps {
  */
 const ConstructionProjectLocationSection: React.FC<
     ConstructionProjectLocationSectionProps
-> = ({ form, enumValues }) => {
+> = ({ form, enumValues, project }) => {
     const { props } = usePage<any>();
 
     // Watch city value for area filtering
@@ -73,7 +75,11 @@ const ConstructionProjectLocationSection: React.FC<
 
             {/* Area / District */}
             <Col xs={24} md={12}>
-                <Form.Item name="area" label="Area / District">
+                <Form.Item
+                    name="area"
+                    label="Area / District"
+                    initialValue={project?.location?.area}
+                >
                     <Select
                         options={areaOptions}
                         placeholder={
